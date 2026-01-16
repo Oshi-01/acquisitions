76e173a06564f11fd52bf479d55343a6093e02ed
Oshi-01 <mozahidul@hubxpert.com>
2026-01-16 21:28:55 +0600

    impentation eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+
+export default [
+  {
+    ignores: ['node_modules/', 'dist/'],
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
+];
